f68f4fbf6c5fb251f896bede8acf307940e9cf44
emmyz00 <109832461+emmyz00@users.noreply.github.com>
2024-09-04 15:45:24 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  ObstacleDetection user_project (
+  tt_um_emmyxu_object_detection user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
