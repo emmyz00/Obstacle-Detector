aac6bb46ac97135b5e3fef2a8a16ab61ef9e09b9
emmyz00 <109832461+emmyz00@users.noreply.github.com>
2024-09-05 20:12:58 -0400

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,12 +15,26 @@ module tt_um_emmyxu_obstacle_detection (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
+wire rst;
+wire sensor_left;
+wire sensor_right;
+wire [1:0] left_buzz;
+wire [1:0] right_buzz;
+    
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
-
+  assign rst = !rst_n;
+  assign sensor_left = ui_in[0];
+  assign sensor_right = ui_in[1];
+  assign left_buzz = uo_out[]
+  assign right_buzz = uo_out[]
+  generate 
+      obstacle_detection u_obstacle_detection(
+          
+      );
+  endgenerate
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
